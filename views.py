7fca6ffd5934b36cd34f64e05ded00c7c34367f8
Nicolas Seinlet <nicolas@seinlet.com>
2024-10-24 20:40:45 +0200

    [FIX] handle wrong emails
    Even when user use wrong email addres, error in sending it
    remove the user profile without crashing.

--- views.py
+++ views.py
@@ -1,3 +1,5 @@
+import logging
+
 from django.contrib.auth import logout
 from django.shortcuts import redirect, render
 from django.http import HttpResponseRedirect, Http404
@@ -8,24 +10,37 @@ from django.utils.translation import gettext as _
 from .forms import UserRegisterForm, MissingTokenForm
 from .models import UserValidation
 
+_logger = logging.getLogger(__name__)
+
 
 class SignUpView(generic.CreateView):
     form_class = UserRegisterForm
     success_url = reverse_lazy("token_sent")
     template_name = "auth-register.html"
 
+    def post(self, request, *args, **kwargs):
+        try:
+            return super().post(request, *args, **kwargs)
+        except:
+            form = UserRegisterForm(request.POST)
+            return render(request, self.template_name, {"form": form, "extra_error": _('Error while creating user. Check email adress or username.')})
+
+
 def missing_token(request):
+    extra_error = ""
     if request.method == "POST":
         form = MissingTokenForm(request.POST)
         if form.is_valid():
             uv = UserValidation.objects.filter(owner_uid__email=form.cleaned_data['email'])
             if uv.count()>0:
-                uv.first().send_mail()
-                return HttpResponseRedirect("/token-resent/")
+                if uv.first().send_mail():
+                    return HttpResponseRedirect("/token-resent/")
+                else:
+                    extra_error = _("There was an issue sending email. Is your address correct?")
     else:
         form = MissingTokenForm()
 
-    return render(request, "missing-token.html", {"form": form})
+    return render(request, "missing-token.html", {"form": form, "extra_error": extra_error})
 
 def logout_view(request):
     logout(request)
