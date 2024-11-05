a473233f1c51bd81a453205eb248aeb9821d2e80
Nicolas Seinlet <nicolas@seinlet.com>
2024-11-05 21:29:29 +0100

    [IMP] Manage 2FA

--- models.py
+++ models.py
@@ -8,6 +8,7 @@ from django.db import models
 from django.template.loader import render_to_string
 from django.utils.html import strip_tags
 from django.utils.translation import gettext as _
+from django_otp.plugins import otp_email, otp_totp
 
 _logger = logging.getLogger(__name__)
 
@@ -34,3 +35,45 @@ class UserValidation(models.Model):
             _logger.error(e)
             return False
         return True
+
+
+def get_otp_device(self):
+    #Check if user have at least email as 2FA
+    device = {
+        'instance': None,
+        'type': False,
+    }
+
+    if otp_totp.models.TOTPDevice.objects.filter(user=self).filter(confirmed=True).count() == 0:
+        if otp_email.models.EmailDevice.objects.filter(user=self).count() == 0:
+            device['instance'] = otp_email.models.EmailDevice.objects.create(user=self, confirmed=True)
+            device['type'] = 'email'
+        else:
+            device['instance'] = otp_email.models.EmailDevice.objects.filter(user=self)[0]
+            device['type'] = 'email'
+    else:
+        device['instance'] = otp_totp.models.TOTPDevice.objects.filter(user=self).filter(confirmed=True)[0]
+        device['type'] = 'totp'
+    if not device['instance']:
+        raise Exception("Could not create OTP device")
+        
+    return device
+
+def ensure_totp_device(self):
+    if otp_totp.models.TOTPDevice.objects.filter(user=self).count() == 0:
+        device = otp_totp.models.TOTPDevice.objects.create(user=self, confirmed=False)
+    else:
+        device = otp_totp.models.TOTPDevice.objects.filter(user=self)[0]
+    if not device:
+        raise Exception("Could not create TOTP device")
+        
+    return device
+
+def reset_totp(self):
+    for device in otp_totp.models.TOTPDevice.objects.filter(user=self).filter(confirmed=True):
+        device.confirmed = False
+        device.save()
+
+User.add_to_class("get_otp_device", get_otp_device)
+User.add_to_class("ensure_totp_device", ensure_totp_device)
+User.add_to_class("reset_totp", reset_totp)
--- views.py
+++ views.py
@@ -1,11 +1,19 @@
 import logging
+import json
 
 from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
+from django.conf import settings
+from django.core.exceptions import ValidationError
+from django.db import transaction
+from django.http import JsonResponse, HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import redirect, render
-from django.http import HttpResponseRedirect, Http404
 from django.urls import reverse_lazy
-from django.views import generic
 from django.utils.translation import gettext as _
+from django.views import generic
+from django_otp import DEVICE_ID_SESSION_KEY
+from django_otp.forms import OTPTokenForm
+from django_otp.qr import write_qrcode_image
 
 from .forms import UserRegisterForm, MissingTokenForm
 from .models import UserValidation
@@ -76,3 +84,81 @@ def activate_account(request, token):
     uid.save()
     user_reg.delete()
     return HttpResponseRedirect('/login/')
+
+@login_required
+def otp(request):
+    def _redirect(request):
+        redirect_url = request.GET.get('next', False)
+        if not redirect_url:
+            redirect_url = settings.LOGIN_REDIRECT_URL
+        return redirect(redirect_url)
+    
+    if request.user.is_verified():
+        return _redirect(request)
+
+    #Ensure user have at least the email device if no other available
+    device = request.user.get_otp_device()
+    form = OTPTokenForm(request.user)
+    error_msg = ""
+    if request.method == "POST":
+        token = False
+        if 'otp_token' in request.POST:
+            token = request.POST.get('otp_token')
+        else:
+            post_data = json.loads(request.body.decode("utf-8"))
+            token = post_data['otp_token']
+        try:
+            with transaction.atomic():
+                form.user.otp_device = form._verify_token(request.user, token, device['instance'])
+                request.session[DEVICE_ID_SESSION_KEY] = form.user.otp_device.persistent_id
+                return _redirect(request)
+        except ValidationError as e:
+            error_msg = e.message
+    else:
+        form = OTPTokenForm(request.user)
+        if device['type'] == 'email':
+            device['instance'].generate_challenge()
+
+    return render(request, "auth-otp.html", {"form": form, "device": device, "error_msg": error_msg})
+
+@login_required
+def otp_new_challenge(request):
+    if request.method == "POST":
+        device = request.user.get_otp_device()
+        if device['type'] == 'email':
+            device['instance'].generate_challenge()
+            return JsonResponse({"generated": True})
+    return JsonResponse({"generated": False})
+
+@login_required
+def otp_totp_qr_code(request):
+    device = request.user.ensure_totp_device()
+    response = HttpResponse(content_type='image/svg+xml')
+    write_qrcode_image(device.config_url, response)
+    return response
+
+@login_required
+def otp_totp_check(request):
+    device = request.user.ensure_totp_device()
+    if request.method == "POST":
+        token = False
+        if 'otp_token' in request.POST:
+            token = request.POST.get('otp_token')
+        else:
+            post_data = json.loads(request.body.decode("utf-8"))
+            token = post_data['otp_token']
+        try:
+            with transaction.atomic():
+                device.confirmed = device.verify_token(token)
+                device.save()
+        except ValidationError as e:
+            error_msg = e.message
+
+    return JsonResponse({"checked": device.confirmed})
+
+
+@login_required
+def otp_totp_settings(request):
+    device = request.user.get_otp_device()
+
+    return JsonResponse({"type": device['type']})
