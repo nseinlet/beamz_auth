cd9637dabe33f1f33b046dd69e76ef52a5c07ce1
Nicolas Seinlet <nicolas@seinlet.com>
2025-03-22 22:27:42 +0100

    [IMP] manage more than one captcha provider
    Turnstile or captchafox.

--- README.md
+++ README.md
@@ -9,3 +9,17 @@ Handle validation of user registration, as well as sessions listing.
 pip install -r requirements.txt
 
 ```
+
+# Captcha
+
+Use a captcha service to prevent spam registrations.
+Tested services are Captchafox and Turnstile
+
+To use them, you need to set the given settings:
+```python
+# settings.py
+CAPTCHA_SERVICE = 'CAPTACHAFOX'  # or 'TURNSTILE'
+CAPTCHA_SERVICE_KEY = 'your_captcha_service_key'
+CAPTCHA_SERVICE_SECRET = 'your_captcha_service_secret'
+```
+if `CAPTCHA_SERVICE` is not set, the captcha will not be used.
--- widgets.py
+++ widgets.py
@@ -3,18 +3,43 @@ from django import forms
 from django.conf import settings
 
 
-class TurnstileInput(forms.Widget):
-    template_name = 'widgets/turnstile.html'
+class CaptchaInput(forms.Widget):
+    template_name = 'widgets/captcha.html'
 
     def value_from_datadict(self, data, files, name):
-        return data.get('cf-turnstile-response')
+        if not hasattr(settings, 'CAPTCHA_SERVICE'):
+            return False
+        if settings.CAPTCHA_SERVICE == 'TURNSTILE':
+            return data.get('cf-turnstile-response')
+        return data.get('cf-captcha-response')
+
+    def _captcha_class(self):
+        if not hasattr(settings, 'CAPTCHA_SERVICE'):
+            return ''
+        if settings.CAPTCHA_SERVICE == 'CAPTCHAFOX':
+            return 'captchafox'
+        if settings.CAPTCHA_SERVICE == 'TURNSTILE':
+            return 'cf-turnstile'
+        return ''
+    
+    def _captcha_jsapiurl(self):
+        if not hasattr(settings, 'CAPTCHA_SERVICE'):
+            return ''
+        if settings.CAPTCHA_SERVICE == 'CAPTCHAFOX':
+            return "https://cdn.captchafox.com/api.js"
+        if settings.CAPTCHA_SERVICE == 'TURNSTILE':
+            return 'https://challenges.cloudflare.com/turnstile/v0/api.js' 
+        return ''
+
 
     def build_attrs(self, base_attrs, extra_attrs=None):
         attrs = super().build_attrs(base_attrs, extra_attrs)
-        attrs['data-sitekey'] = settings.TURNSTILE_SITEKEY
+        attrs['data-sitekey'] = settings.CAPTCHA_SERVICE_SITEKEY
         return attrs
 
     def get_context(self, name, value, attrs):
         context = super().get_context(name, value, attrs)
-        context['api_url'] = settings.TURNSTILE_JSAPIURL
+        context['api_url'] = self._captcha_jsapiurl()
+        context['captcha_class'] = self._captcha_class()
         return context
+    
